4c89bfdc54bc40d0a5d7d0aec6e447d1b1093cbe
Ebtisam Jubair <ebtisam-jubair@users.noreply.github.com>
2026-03-16 08:57:14 +0600

    feat: implement markAsReadAll for bulk inbox marking

---
+++ src/http/mark_as_read_all.py
@@ -0,0 +1,16 @@
+from typing import Any
+from ..utils.utils import parse_and_check_login
+
+async def mark_as_read_all(post_func, ctx: Any):
+    form = {
+        "folder": 'inbox'
+    }
+    
+    url = "https://www.facebook.com/ajax/mercury/mark_folder_as_read.php"
+    res = await post_func(url, ctx, form)
+    res_data = parse_and_check_login(ctx, res)
+    
+    if res_data and res_data.get("error"):
+        raise Exception(f"markAsReadAll error: {res_data['error']}")
+        
+    return res_data
