67be2a8b302669bf44013db5f736d2fc6cd5d4c1
Ebtisam Jubair <ebtisam-jubair@users.noreply.github.com>
2026-03-16 08:57:15 +0600

    feat: implement unsendMessage as modular service

---
+++ src/http/unsend_message.py
@@ -0,0 +1,7 @@
+from typing import Any
+from ..utils.utils import parse_and_check_login
+
+async def unsend_message(post_func, ctx: Any, message_id: str):
+    form = {"message_id": message_id}
+    res = await post_func("https://www.facebook.com/messaging/unsend_message/", ctx, form)
+    return parse_and_check_login(ctx, res)
