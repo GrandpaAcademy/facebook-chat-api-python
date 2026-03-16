55017010b5ac566da74a98a6d1cecb11244b0d24
Ebtisam Jubair <ebtisam-jubair@users.noreply.github.com>
2026-03-16 08:56:45 +0600

    feat: implement unfriend to remove users from friend list

---
+++ src/http/unfriend.py
@@ -0,0 +1,19 @@
+from typing import Any
+from ..utils.utils import parse_and_check_login
+
+async def unfriend(post_func, ctx: Any, user_id: str):
+    form = {
+        "uid": str(user_id),
+        "unref": "bd_friends_tab",
+        "floc": "friends_tab",
+        "nctr[_mod]": f"pagelet_timeline_app_collection_{ctx.user_id}:2356318349:2"
+    }
+    
+    url = "https://www.facebook.com/ajax/profile/removefriendconfirm.php"
+    res = await post_func(url, ctx, form)
+    res_data = parse_and_check_login(ctx, res)
+    
+    if res_data and res_data.get("error"):
+        raise Exception(f"unfriend error: {res_data['error']}")
+        
+    return res_data
